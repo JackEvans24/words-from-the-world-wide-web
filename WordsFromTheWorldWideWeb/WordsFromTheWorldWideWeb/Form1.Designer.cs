7eb46e58441ff02d211f188627446b3266d01e52
Jack Evans <jackevans24@gmail.com>
2018-09-07 16:32:23 +0100

    initial commit of WordClassBuilder and WordsFromTheWorldWideWeb solutions.

---
+++ WordsFromTheWorldWideWeb/WordsFromTheWorldWideWeb/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WordsFromTheWorldWideWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartStopButton = new System.Windows.Forms.Button();
+            this.DisplayTextbox = new System.Windows.Forms.RichTextBox();
+            this.WordInfoLabel = new System.Windows.Forms.Label();
+            this.TermInfoLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartStopButton
+            // 
+            this.StartStopButton.BackColor = System.Drawing.Color.White;
+            this.StartStopButton.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartStopButton.ForeColor = System.Drawing.Color.Teal;
+            this.StartStopButton.Location = new System.Drawing.Point(150, 377);
+            this.StartStopButton.Name = "StartStopButton";
+            this.StartStopButton.Size = new System.Drawing.Size(140, 39);
+            this.StartStopButton.TabIndex = 0;
+            this.StartStopButton.Text = "Start";
+            this.StartStopButton.UseVisualStyleBackColor = false;
+            this.StartStopButton.Click += new System.EventHandler(this.StartStopButton_Click);
+            // 
+            // DisplayTextbox
+            // 
+            this.DisplayTextbox.BackColor = System.Drawing.Color.White;
+            this.DisplayTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayTextbox.ForeColor = System.Drawing.Color.Teal;
+            this.DisplayTextbox.Location = new System.Drawing.Point(13, 88);
+            this.DisplayTextbox.Name = "DisplayTextbox";
+            this.DisplayTextbox.ReadOnly = true;
+            this.DisplayTextbox.Size = new System.Drawing.Size(398, 274);
+            this.DisplayTextbox.TabIndex = 1;
+            this.DisplayTextbox.Text = "";
+            // 
+            // WordInfoLabel
+            // 
+            this.WordInfoLabel.AutoSize = true;
+            this.WordInfoLabel.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WordInfoLabel.ForeColor = System.Drawing.Color.Teal;
+            this.WordInfoLabel.Location = new System.Drawing.Point(108, 11);
+            this.WordInfoLabel.Name = "WordInfoLabel";
+            this.WordInfoLabel.Size = new System.Drawing.Size(197, 24);
+            this.WordInfoLabel.TabIndex = 2;
+            this.WordInfoLabel.Text = "Pulled 0 unique words";
+            // 
+            // TermInfoLabel
+            // 
+            this.TermInfoLabel.AutoSize = true;
+            this.TermInfoLabel.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TermInfoLabel.ForeColor = System.Drawing.Color.Teal;
+            this.TermInfoLabel.Location = new System.Drawing.Point(77, 35);
+            this.TermInfoLabel.Name = "TermInfoLabel";
+            this.TermInfoLabel.Size = new System.Drawing.Size(259, 24);
+            this.TermInfoLabel.TabIndex = 3;
+            this.TermInfoLabel.Text = "using 0 different search terms";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.WordInfoLabel);
+            this.panel1.Controls.Add(this.TermInfoLabel);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 69);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(424, 428);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DisplayTextbox);
+            this.Controls.Add(this.StartStopButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Words from the Web";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartStopButton;
+        private System.Windows.Forms.RichTextBox DisplayTextbox;
+        private System.Windows.Forms.Label WordInfoLabel;
+        private System.Windows.Forms.Label TermInfoLabel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WordsFromTheWorldWideWeb/WordsFromTheWorldWideWeb/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace WordsFromTheWorldWideWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> randomWords = new List<string>();
+        List<string> searchTerms = new List<string>();
+
+        public bool isRunning = false;
+        private async void StartStopButton_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                StartStopButton.Text = "Stop";
+
+                await Task.Run(() =>
+                {
+                    while (isRunning)
+                    {
+                        string randomWord = GetWord();
+
+                        if (randomWord.Length > 0)
+                        {
+                            UpdateTextbox(randomWord);
+                            UpdateLabel();
+                        }
+                    }
+
+                    UpdateButtonText();
+                });
+            }
+            else
+            {
+                isRunning = false;
+                StartStopButton.Text = "Stopping...";
+            }
+        }
+        
+        private string GetWord()
+        {
+            Random rnd = new Random();
+
+            IWebDriver driver = new ChromeDriver();
+            string searchString = RandomWord.Get();
+
+            driver.Url = "http://www.google.co.uk";
+
+            driver.FindElement(By.Name("q")).SendKeys(searchString + OpenQA.Selenium.Keys.Return);
+
+            /*
+            try
+            {
+                IList<IWebElement> h3Links = driver.FindElements(By.TagName("h3"));
+
+                h3Links[2].FindElement(By.TagName("a")).Click();
+            }
+            catch (InvalidOperationException)
+            {
+                driver.Quit();
+                return "";
+            }
+            catch (NoSuchElementException)
+            {
+                driver.Quit();
+                return "";
+            }
+            */
+
+            List<IWebElement> spans = new List<IWebElement>();
+            IWebElement bigSpan;
+            try
+            {
+                spans = driver.FindElements(By.ClassName("st")).OrderByDescending(p => p.Text.Length).ToList();
+                switch (rnd.Next(1, 6))
+                {
+                    default:
+                        bigSpan = spans[0];
+                        break;
+                    case 2:
+                        bigSpan = spans[1];
+                        break;
+                    case 3:
+                        bigSpan = spans[2];
+                        break;
+                    case 4:
+                        bigSpan = spans[3];
+                        break;
+                    case 5:
+                        bigSpan = spans[4];
+                        break;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                driver.Quit();
+                return "";
+            }
+            catch (NoSuchElementException)
+            {
+                driver.Quit();
+                return "";
+            }
+
+            char[] space = { ' ', '\r', '\n', '\t', '.', ',', '"', '\'', '(', ')', '/', '\\', '|', '<', '>' , '^', '&', '*', ':', ';', '@', '#', '~', '•'};
+            string[] allWords = bigSpan.Text.Split(space, StringSplitOptions.RemoveEmptyEntries);
+
+            string randomWord = String.Empty;
+            if (allWords.Length > 1)
+                randomWord = allWords[rnd.Next(0, allWords.Length - 1)].ToLowerInvariant();
+
+            if (!searchTerms.Contains(searchString))
+                searchTerms.Add(searchString);
+
+            if (!randomWords.Contains(randomWord))
+                randomWords.Add(randomWord);
+
+            driver.Quit();
+            return randomWord;
+        }
+
+        public void UpdateTextbox(string text)
+        {
+            BeginInvoke((MethodInvoker)delegate 
+                { DisplayTextbox.AppendText(text + " ");
+                    DisplayTextbox.Refresh();
+                    DisplayTextbox.Parent.Refresh();
+                });
+        }
+
+        public void UpdateLabel()
+        {
+            BeginInvoke((MethodInvoker)delegate 
+                {   WordInfoLabel.Text = String.Format("Pulled {0} unique words", randomWords.Count);
+                    TermInfoLabel.Text = String.Format("using {0} different search terms", searchTerms.Count);
+                    DisplayTextbox.Refresh();
+                    DisplayTextbox.Parent.Refresh();
+                });
+        }
+
+        public void UpdateButtonText()
+        {
+            BeginInvoke((MethodInvoker)delegate
+            {
+                StartStopButton.Text = "Start";
+                StartStopButton.Refresh();
+                DisplayTextbox.Parent.Refresh();
+            });
+
+        }
+    }
+}
